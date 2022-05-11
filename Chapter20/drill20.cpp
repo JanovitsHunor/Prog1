54ac4d78e186065fdc469a87597a43a9dfcfe722
JanovitsHunor <91329017+JanovitsHunor@users.noreply.github.com>
2022-05-11 12:43:14 +0200

    Update drill20.cpp

--- Chapter20/drill20.cpp
+++ Chapter20/drill20.cpp
@@ -1,12 +1,12 @@
 #include "std_lib_facilities.h"
 #include <iterator>
 
-template<typename Iter1, typename Iter2>
+template<typename Iter1, typename Iter2> // iterator: olyan objektum, amely mutat egy elemre, legegyszer≈±bb form√°ja a pointer - Feladat #6
 Iter2 IterCopy(Iter1 f1, Iter1 e1, Iter2 f2)
 {
 	while (f1 != e1)
 	{
-		*f2 = *f1;
+		*f2 = *f1; //f2 mem√≥riahely √©rt√©ke legyen egyenl≈ë f1-gyel, majd l√©ptet√ºnk (++)
 		++f1;
 		++f2;
 	}
@@ -16,16 +16,16 @@ Iter2 IterCopy(Iter1 f1, Iter1 e1, Iter2 f2)
 
 int main()
 {
-	// intek tˆmbje
+	// intek t√∂mbje - Feladat #1
 	int intArray[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// intek vektora
+	// intek vektora - Feladat #2
 	vector<int> intVector{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// intek list·ja
+	// intek list√°ja - Feladat #3
 	list<int> intList{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	// m·solatok
+	// m√°solatok - Feladat #4
 	int intArray2[10];
 	copy(begin(intArray), end(intArray), begin(intArray2));
 
@@ -33,7 +33,7 @@ int main()
 
 	list<int> intList2 = intList;
 
-	// ÈrtÈkek nˆvelÈse
+	// √©rt√©kek n√∂vel√©se - Feladat #5
 	for (int& i : intArray)
 		i += 2;
 
@@ -43,7 +43,7 @@ int main()
 	for (int& i : intList)
 		i += 5;
 
-	// IterCopy oper·ciÛ tesztelÈse
+	// IterCopy oper√°ci√≥ tesztel√©se // Feladat #7
 	IterCopy(intArray.begin(), intArray.end(), intVector2.begin());
 	if (intArray[0] != intVector2[0])
 		cout << "Error: intArray[0] && intVector2[0] are not the same!" << endl;
@@ -52,7 +52,7 @@ int main()
 	if (*(intList.begin()) != intArray2[0])
 		cout << "Error: intList[0] && intArray2[0] are not the same!" << endl;
 
-	// a 3 megtal·l·sa a vektorban
+	// a 3 megtal√°l√°sa a vektorban - // Feladat #8
 	auto vecResult = find(begin(intVector), end(intVector), 3);
 	if (vecResult != end(intVector))
 		cout << "intVector contains 3 at position " << distance(intVector.begin(), vecResult) << endl;
@@ -60,7 +60,7 @@ int main()
 		cout << "intVector does not contain 3" << endl;
 	intVector.push_back(3);
 
-	// a 27-es megtal·l·sa a list·ban
+	// a 27-es megtal√°l√°sa a list√°ban - // Feladat #8
 	auto listResult = find(begin(intList), end(intList), 27);
 	if (listResult != end(intList))
 		cout << "listResult contains 27 at position " << distance(intList.begin(), listResult) << endl;
